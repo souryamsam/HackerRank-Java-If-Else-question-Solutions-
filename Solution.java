cd0fef31d084dcbe7e00da0dc2730bc1823dff46
Sourayam Sam <108325634+souryamsam@users.noreply.github.com>
2022-10-11 14:22:43 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        Scanner c=new Scanner(System.in);
+        int n= c.nextInt();
+        if(n%2==0)
+        {
+            if(n>=2 && n<=5)
+            {
+                System.out.println("Not weird");
+            }
+            else if(n>=6 && n<=20)
+            {
+                System.out.println("Weird");
+            }else
+            {
+                System.out.println("Not Weird");
+            }
+        }
+        else
+        {
+            System.out.println("Weird");
+        }
+    }
+}
